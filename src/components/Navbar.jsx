b6705ccb4143ec1d443cfd6b09965558b84d36b5
simon <simon.chambart@student.hogent.be>
2024-05-31 23:30:24 +0200

    Add icons and colors to navbar and hompage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   const {isAuthed} = useAuth();
 
   return (
-    <nav className={`navbar sticky-top mb-4`}>
+    <nav className="navbar p-4 sticky-top text-white bg-success">
       <div className="container-fluid flex-column flex-sm-row align-items-start align-items-sm-center">
         {
           isAuthed
@@ -15,18 +15,22 @@ export default function Navbar() {
           </div>
         )
         : (
-          <>
+
           <div className="nav-item my-2 mx-sm-3 my-sm-0">
-            <Link className="nav-link" to="/login">Login</Link>
+            <Link className="nav-link" to="/login">
+                <i className="bi bi-person-circle" />
+              </Link>
           </div>
-          </>
+
         )
         }
         <div className="nav-item my-2 mx-sm-3 my-sm-0">
           <Link className="nav-link" to="/home">Logo</Link>
         </div>
         <div className="nav-item my-2 mx-sm-3 my-sm-0">
-          <Link className="nav-link" to="/basket">Basket</Link>
+        <Link className="nav-link" to="/basket">
+            <i className="bi bi-cart2" />
+          </Link>
         </div>
       </div>
     </nav>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-// pages/Home.jsx
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
 
 function Home() {
   return (
-    <div>
-      <h2>Welcome to the Home Page</h2>
-      <p>This is a simple home page.</p>
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-info">
+      <div className="d-flex flex-column align-items-center">
+        <button type="button" className="btn btn-warning rounded-circle mb-3 circular-button" >Button 1</button>
+        <button type="button" className="btn btn-warning rounded-circle mb-3 circular-button">Button 2</button>
+        <button type="button" className="btn btn-warning rounded-circle circular-button" >Button 3</button>
+      </div>
     </div>
   );
 }
